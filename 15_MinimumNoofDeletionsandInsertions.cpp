5e3d068800f7d5931aa8cf5b9a3a283038db31c7
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-09-17 12:43:51 +0530

    5. LCS Pattern (GFG-MinmNoofDeletions&Insertions)

--- 15_MinimumNoofDeletionsandInsertions.cpp
+++ 15_MinimumNoofDeletionsandInsertions.cpp
@@ -26,7 +26,7 @@ int minOperations(string str1, string str2)
 		            if(str1[i-1]==str2[j-1])
 		                dp[i][j]= 1 + dp[i-1][j-1];
 		            else
-		                dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+		                dp[i][j]= max(dp[i-1][j], dp[i][j-1]); 
 		        }
 		    }
 		  
